18de120abd62579dbb2f38c8d07fcab1505a3c0c
nicolaiharbo <nicolai@famharbo.dk>
2016-05-31 10:57:39 +0200

    fixed

--- www/js/app.js
+++ www/js/app.js
@@ -3,8 +3,13 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'ngCordova'])
+angular.module('starter', ['ionic', 'ngCordova']) //ngCordova injectes
 
+  .constant('ApiEndpoint', { //Tager hånd om CORS, clientside - der er sat en proxy op i "ionic.project"-filen.
+    url: 'http://localhost:3000/api' //Skal ændres til det rigtige, ved deployment.
+  })
+
+  //Keyboard-settings
   .run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
       if (window.cordova && window.cordova.plugins.Keyboard) {
@@ -22,6 +27,8 @@ angular.module('starter', ['ionic', 'ngCordova'])
       }
     });
   })
+
+  //route/state provider - sætter mappet ind fra et directive (templates)
   .config(function ($stateProvider, $urlRouterProvider) {
 
     $stateProvider
@@ -34,6 +41,8 @@ angular.module('starter', ['ionic', 'ngCordova'])
     $urlRouterProvider.otherwise("/");
 
   })
+
+  //Controller for map - cordovaGeolocation er injected.
   .controller('MapCtrl', function ($scope, $state, $cordovaGeolocation, $ionicModal, $http) {
     var options = {timeout: 10000, enableHighAccuracy: true};
 
@@ -51,6 +60,7 @@ angular.module('starter', ['ionic', 'ngCordova'])
 
       console.log("myLat:" + myLat); //DET STØRSTE TAL (1)
 
+      //Opsætning af map fra start
       var mapOptions = {
         center: latLng,
         zoom: 15,
@@ -59,11 +69,13 @@ angular.module('starter', ['ionic', 'ngCordova'])
       console.log("myLat--- " + myLat);
       console.log("myLng--- " + myLng);
 
-      $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+      //Her hentes mappet ind..
+      $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions); //id "map" er defineret i templaten.
 
       //Wait until the map is loaded, this adds a marker to the center og the map, where we're located.
       google.maps.event.addListenerOnce($scope.map, 'idle', function () {
 
+        //Opsætning af marker
         var marker = new google.maps.Marker({
           map: $scope.map,
           animation: google.maps.Animation.DROP,
@@ -76,6 +88,7 @@ angular.module('starter', ['ionic', 'ngCordova'])
           content: "Here I am!"
         });
 
+        //Eventlistener til click-funktionen.
         google.maps.event.addListener(marker, 'click', function () {
           infoWindow.open($scope.map, marker);
         });
@@ -120,10 +133,11 @@ angular.module('starter', ['ionic', 'ngCordova'])
       user.loc.push(myLat);
       console.log(JSON.stringify(user));
 
+      //Post til Lars' backend - virker fordi han har håndteret CORS på backenden, med cors-pakken eller ALLOW-ORIGIN * -tingen.
       $http({
         method: "POST",
         url: "http://ionicbackend-plaul.rhcloud.com/api/friends/register/" + user.distance,
-        data: user
+        data: user //useren gives med i body.
       }).then(function (res, req) {
         console.log(res);
 
@@ -132,39 +146,31 @@ angular.module('starter', ['ionic', 'ngCordova'])
 
           for (var i = 0; i < res.data.length; i++) {
 
-            google.maps.event.addListenerOnce($scope.map, 'idle', function () {
+            //find kordinat til markeren, fra db's lat or long
+            var latLng = new google.maps.LatLng(parseFloat(res.data[i].loc[1]), parseFloat(res.data[i].loc[0]));
 
-              var latLng = new google.maps.LatLng(parseFloat(res.data[i].loc[1]), parseFloat(res.data[i].loc[0]));
-
-              //Create a marker in the users LatLng
-              var marker = new google.maps.Marker({
-                map: $scope.map,
-                animation: google.maps.Animation.DROP,
-                icon: new google.maps.MarkerImage("http://maps.google.com/mapfiles/ms/icons/" + "red.png"),
-                position: latLng
-              });
-
-              //adds popup with location info. (the "content")
-              var infoWindow = new google.maps.InfoWindow({
-                content: res.data[i].userName
-              });
-
-              //Shows the stuff when the marker is clicked
-              google.maps.event.addListener(marker, 'click', function () {
-                infoWindow.open($scope.map, marker);
-              });
-
-            }, function (error) {
-              console.log("Could not get location");
+            //Create a marker in the users LatLng
+            var marker = new google.maps.Marker({
+              map: $scope.map,
+              animation: google.maps.Animation.DROP,
+              icon: new google.maps.MarkerImage("http://maps.google.com/mapfiles/ms/icons/" + "red.png"),
+              position: latLng
             });
+
+            //adds popup with location info. (the "content")
+            var infoWindow = new google.maps.InfoWindow({
+              content: res.data[i].userName
+            });
+
+            //Shows the stuff when the marker is clicked
+            google.maps.event.addListener(marker, 'click', function () {
+              infoWindow.open($scope.map, marker);
+            });
+
           }
         } else {
           alert("No friends found in selected distence!")
         }
-
-
       })
     };
-
-
   });
